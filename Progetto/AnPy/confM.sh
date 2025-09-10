6004f1ebd490bc7df1d313777b1af8ba2111e93e
Negrini085 <filippo.negrini@studenti.unimi.it>
2025-09-10 11:47:45 +0200

    Ottenuta versione 2 ottimizzata

--- Progetto/AnPy/confM.sh
+++ Progetto/AnPy/confM.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-modG=("line1_v1.npy" "line1_v2.npy" "line2_v1.npy" "line2_v2.npy")      	# Nome del file di output previsioni
-titM=("line1_v1" "line1_v2" "line2_v1" "line2_v2")	               		# Nome output matrice di confusione
+modG=("line3_v2.npy")      	# Nome del file di output previsioni
+titM=("line3_v2")	               		# Nome output matrice di confusione
 
 #-----------------------------------------------------------------------#
 #       Ciclo per modificare parametri esecuzione script confM.py       #
--- Progetto/AnPy/guess.sh
+++ Progetto/AnPy/guess.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-modN=("line1_v1.keras" "line1_v2.keras" "line2_v1.keras" "line2_v2.keras")      	             # Nome del file di input modello
-modG=("line1_v1.npy" "line1_v2.npy" "line2_v1.npy" "line2_v2.npy")      	                     # Nome del file di output previsioni
+modN=("line3_v2.keras")      	             # Nome del file di input modello
+modG=("line3_v2.npy")      	                     # Nome del file di output previsioni
 
 #-----------------------------------------------------------------------#
 #       Ciclo per modificare parametri esecuzione script guess.py       #
